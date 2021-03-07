9b9a7589ff3746676bfbc918b31885cc36d0638b
dgosling <daniellgosling@gmail.com>
2021-03-06 17:00:08 -0700

    Initial commit

---
+++ Assets/animation_test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation_test : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    Animator Animator;
+    private int layer;
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+           layer = Animator.GetLayerIndex("Gesture, Override");
+            Animator.PlayInFixedTime("chargeStart", layer,0f);
+        }
+    }
+}
